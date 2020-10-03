f8ff110de55c51093051f16845fa10502062cb8e
Sherin Varughese <sherin.a.varughese@gmail.com>
2020-10-03 13:14:07 -0400

    build my portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Sherin Varughese', // Title for your website.
     tagline: 'A data aficionado, in a continuous learning loop',
     profilePhoto: 'img/profile.png',
-    url: 'https://SherinV.github.io', // Your website URL
+    url: 'https://sherinv.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'SherinV',
+    organizationName: 'sherinv',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
